af4119c065d3dda69b1697b96716da79959edcbe
BaoGeist <linofbaoze@outlook.com>
2024-04-01 22:39:38 -0400

    feat: added Button component

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
 
 export default function App() {
+
+  const handlePress = () => {
+    console.log("button pressed");
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={handlePress} buttonText="test"/>
     </View>
   );
 }
