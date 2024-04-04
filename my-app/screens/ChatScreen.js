6bae08ea1e95bf6862f7cd539831d1b59ee4b77a
anjola-a <adewaa1@mcmaster.ca>
2024-04-04 01:10:03 -0400

    dummy data

--- my-app/screens/ChatScreen.js
+++ my-app/screens/ChatScreen.js
@@ -1,15 +1,31 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import MessageList from '../components/MessageList'; 
 
 const ChatScreen = ({route}) => {
 
     const { chatId } = route.params;
+    const [messages, setMessages] = useState([
+    { sender: 'Alice', body: 'Hi there!' },
+    { sender: 'Bob', body: 'Hey! How are you?' },
+    { sender: 'Alice', body: "I'm doing well, thanks!" }
+    ]);
         
+   
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1}}>
+        <View style={{ padding: 50 }}>
             <Text>Welcome to chat {chatId}! </Text>
         </View>
-    )
-}
+        <View style={{ flex: 1, padding: 20 }}>
+            <MessageList messages={messages} />
+        </View>
+        <View style={{ padding: 50 }}>
+            <Text> hi</Text>
+            {/* <MessageInput onSendMessage={handleSendMessage} /> */}
+        </View>
+        </View>
+    );
+};
 
 export default ChatScreen
